3a4fce9314d851dbf9476607392a1e8e7aa6f6be
Alex Gallant <agallant1201@gmail.com>
2020-02-04 13:33:29 -0700

    Remove unused pet variable

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -10,7 +10,7 @@ class PetsController < ApplicationController
 
   def create
     shelter = Shelter.find(params[:shelter_id])
-    pet = shelter.pets.create(pet_params)
+    shelter.pets.create(pet_params)
     redirect_to "/shelters/#{shelter.id}/pets"
   end
 
