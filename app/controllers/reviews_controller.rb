e8d9b7c0d0968e47aec1ebaff14f13e043bcd430
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-02-04 15:28:35 -0700

    Update the update action method in review controller
    Co-Authored-By: agallant121 <52808022+agallant121@users.noreply.github.com>

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ReviewsController < ApplicationController
   def new
     @shelter = Shelter.find(params[:shelter_id])
@@ -7,10 +9,10 @@ class ReviewsController < ApplicationController
     shelter = Shelter.find(params[:shelter_id])
     new_review = shelter.reviews.create(review_params)
     if new_review.save
-      flash[:happy] = "Review successfully submitted!"
+      flash[:happy] = 'Review successfully submitted!'
       redirect_to "/shelters/#{shelter.id}"
     else
-      flash[:sad] = "Failed to fill out require information."
+      flash[:sad] = 'Failed to fill out require information.'
       redirect_to "/shelters/#{shelter.id}/reviews/new"
     end
   end
@@ -23,7 +25,13 @@ class ReviewsController < ApplicationController
     shelter = Shelter.find(params[:shelter_id])
     review = shelter.reviews.find(params[:review_id])
     review.update(review_params)
-    redirect_to "/shelters/#{shelter.id}"
+    if review.save
+      flash[:happy] = 'Review successfully updated!'
+      redirect_to "/shelters/#{shelter.id}"
+    else
+      flash[:sad] = 'Please enter all required information'
+      redirect_to "/shelters/#{shelter.id}/reviews/#{review.id}/edit"
+    end
   end
 
   private
