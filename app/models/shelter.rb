b2d03f12b21c743b309e7732546b6c03c659e5c0
Alex Gallant <agallant1201@gmail.com>
2020-02-04 08:44:09 -0700

    Add dependent destroy on shelters for reviews
    Co-Authored-By: adumortier <adumortier@users.noreply.github.com>

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -2,5 +2,5 @@ class Shelter < ApplicationRecord
   validates_presence_of :name, :address, :city, :state, :zip
 
   has_many :pets, :dependent => :destroy
-  has_many :reviews
+  has_many :reviews, :dependent => :destroy  #why is this here and not on reviews?
 end
