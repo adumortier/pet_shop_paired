ffcfebc24004422b05caed9ad160365af100d195
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-02-07 14:47:53 -0700

    Add a favorites controller and the update action method to add favorite pets
    Co-Authored-By: agallant121 <52808022+agallant121@users.noreply.github.com>

--- app/controllers/favorite_controller.rb
+++ app/controllers/favorite_controller.rb
@@ -1,11 +1,14 @@
 class FavoriteController < ApplicationController
 
   def update
+    # favorites.add_pet(params[:id])
+    # pet = Pet.find(params[:id])
+
     pet = Pet.find(params[:pet_id])
     pet_id_str = pet.id.to_s
-    session[:fav_pets] ||= Hash.new()
-    session[:fav_pets][pet_id_str] ||= 0
-    require "pry"; binding.pry
+    session[:fav_pets] ||= Hash.new(0)
+    session[:fav_pets][pet_id_str] ||= {}
+    
     flash[:notice] = "You added #{pet.name} to your favorites."
     redirect_to '/pets'
   end
