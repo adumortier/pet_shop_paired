afa34846188f8c0ba59cc38bf8b389cc16324d49
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-02-08 14:02:18 -0700

    Add relations for ApplicationPets and Applications
    Co-Authored-By: agallant121 <52808022+agallant121@users.noreply.github.com>

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,12 +2,15 @@ class Pet < ApplicationRecord
   validates_presence_of :image, :name, :age, :sex
 
   belongs_to :shelter
+  has_many :application_pets
+  has_many :applications, through: :application_pets
 
   def adoption_status
       if adopted?
         "Adopt me!"
       else
         "I'm going home with someone!"
-    end
+      end
   end
+  
 end
