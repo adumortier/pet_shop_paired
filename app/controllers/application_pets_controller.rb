0ff089cb61b4d124f9ab4d5126990d040a50fb0a
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-02-09 10:47:46 -0700

    Add update method to change the status of a pet when application has been approved
    Co-Authored-By: agallant121 <52808022+agallant121@users.noreply.github.com>

--- app/controllers/application_pets_controller.rb
+++ app/controllers/application_pets_controller.rb
@@ -5,4 +5,10 @@ class ApplicationPetsController < ApplicationController
     @applications = pet.applications
   end
 
+  def update
+    pet = Pet.find(params[:pet_id])
+    pet.update(adopted?: "Pending")
+    redirect_to "/pets/#{pet.id}"
+  end
+
 end
