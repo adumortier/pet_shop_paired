25c07e7a413a56b0d17d4afb3e99c0922a3f1f89
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-02-09 13:57:33 -0700

    Add a conditional based on the adoptable status of pet
    Co-Authored-By: agallant121 <52808022+agallant121@users.noreply.github.com>

--- app/controllers/application_pets_controller.rb
+++ app/controllers/application_pets_controller.rb
@@ -7,11 +7,16 @@ class ApplicationPetsController < ApplicationController
 
   def update
     pet = Pet.find(params[:pet_id])
-    pet.update(adopted?: "Pending")
-    redirect_to "/pets/#{pet.id}"
+    if pet.adopted? == "Adoptable"
+      pet.update(adopted?: "Pending")
+      redirect_to "/pets/#{pet.id}"
+    else
+      pet.update(adopted?: "Adoptable")
+      redirect_to "/applications/#{params[:app_id]}" 
+    end  
   end
 
-   def batch_update 
+  def batch_update 
     pet_ids = params[:pet_ids]
     pet_ids.each do |id|
       pet = Pet.find(id)
