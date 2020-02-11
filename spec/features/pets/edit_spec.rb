d11f8596de333b5bdc48e8142da42450fef54595
Alex Gallant <agallant1201@gmail.com>
2020-02-11 16:03:50 -0700

    Add test for flash messages when missing fields on a pet's edit page
    Co-Authored-By: adumortier <adumortier@users.noreply.github.com>

--- spec/features/pets/edit_spec.rb
+++ spec/features/pets/edit_spec.rb
@@ -35,4 +35,18 @@ RSpec.describe "As a visitor" do
     expect(page).to have_content("Burnadette")
     expect(page).to_not have_content("Penny")
   end
+
+  it "shows flash message for specific fields missing when incompletely editing a pet" do
+    click_link "Update #{@penny.name}"
+
+    fill_in :image, with: 'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12234558/Chinook-On-White-03.jpg'
+    fill_in :name, with: "Peewee"
+    fill_in :description, with: "Lively pup"
+    fill_in :age, with: ""
+    fill_in :sex, with: ""
+
+    click_button "Update #{@penny.name}"
+    expect(page).to have_content("Age can't be blank and Sex can't be blank")
+    expect(current_path).to eq("/shelters/#{@shelter.id}/pets/#{@penny.id}/edit")
+  end
 end
