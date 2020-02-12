2906d883c246bf370ced7a5b75986f4cb58bb91f
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-02-12 07:58:42 -0700

    Add a helper method that checks if a rating is between 1 and 5
    Co-Authored-By: agallant121 <52808022+agallant121@users.noreply.github.com>

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,8 @@ class Review < ApplicationRecord
   validates_presence_of :title, :rating, :content, :image
 
   belongs_to :shelter
+
+  def self.valid_rating?(score)
+    (score.to_i > 0) &&  (score.to_i < 6)
+  end
 end
