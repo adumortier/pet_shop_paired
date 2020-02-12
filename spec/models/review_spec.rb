0647a1507e17ae9e46cd90cd03965856347e2748
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-02-12 07:50:19 -0700

    Add a test for the helper method that checks that a rating is valid
    Co-Authored-By: agallant121 <52808022+agallant121@users.noreply.github.com>

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -11,4 +11,20 @@ RSpec.describe Review, type: :model do
   describe "relationships" do
     it {should belong_to :shelter}
   end
+
+  describe "methods" do
+    it "can check that a rating is valid" do 
+      
+      rating1 = 4 
+      expect(Review.valid_rating?(rating1)).to eq(true)
+
+      rating2 = -3 
+      expect(Review.valid_rating?(rating2)).to eq(false)
+
+      rating3 = 6 
+      expect(Review.valid_rating?(rating3)).to eq(false)
+    end
+
+  end
+
 end
