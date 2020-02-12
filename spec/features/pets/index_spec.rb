9caecb4f517b2b84dc5146d60b49c6c7d61ac3fc
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-02-11 20:40:46 -0700

    Add the description field to each pet of the setup
    Co-Authored-By: agallant121 <52808022+agallant121@users.noreply.github.com>

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -15,12 +15,14 @@ RSpec.describe "As a visitor" do
       name: "Penny",
       age: 2,
       sex: "F",
+      description: "huge"
       )
     @paulie = Pet.create(
       image: 'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12234558/Chinook-On-White-03.jpg',
       name: "Paulie",
       age: 3,
       sex: "F",
+      description: "huge"
       )
 
     @shelter.pets << [@penny, @paulie]
@@ -68,12 +70,14 @@ RSpec.describe "As a visitor" do
       name: "Penny",
       age: 2,
       sex: "F",
+      description: "huge"
       )
     @paulie = Pet.create(
       image: 'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12234558/Chinook-On-White-03.jpg',
       name: "Paulie",
       age: 3,
       sex: "F",
+      description: "huge"
       )
 
     @shelter.pets << [@penny, @paulie]
