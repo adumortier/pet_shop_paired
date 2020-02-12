b4260daa2c1dd5612a861b99519d835264eab019
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-02-11 20:39:50 -0700

    Remove the pet from the setup and add the selection of the sex from a drop down
    Co-Authored-By: agallant121 <52808022+agallant121@users.noreply.github.com>

--- spec/features/pets/new_spec.rb
+++ spec/features/pets/new_spec.rb
@@ -10,14 +10,6 @@ RSpec.describe "As a visitor" do
       zip: "80210"
       )
 
-    @penny = Pet.create(
-      image: 'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12234558/Chinook-On-White-03.jpg',
-      name: "Penny",
-      age: 2,
-      sex: "F",
-      )
-    @shelter.pets << @penny
-
     visit "/shelters/#{@shelter.id}/pets"
   end
 
@@ -30,14 +22,14 @@ RSpec.describe "As a visitor" do
     fill_in :name, with: "Peewee"
     fill_in :description, with: "Lively pup"
     fill_in :age, with: 3
-    fill_in :sex, with: "Female"
+    select 'F', :from => :sex
 
     click_button "Create Pet"
     expect(current_path).to eq("/shelters/#{@shelter.id}/pets")
     expect(page).to have_content("Peewee")
     expect(page).to have_content("Lively pup")
     expect(page).to have_content(3)
-    expect(page).to have_content("Female")
+    expect(page).to have_content("F")
   end
 
   it "shows flash message if new pet form is filled out incorrectly" do
@@ -47,12 +39,12 @@ RSpec.describe "As a visitor" do
 
     fill_in :image, with: 'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12234558/Chinook-On-White-03.jpg'
     fill_in :name, with: "Peewee"
-    fill_in :description, with: "Lively pup"
+    fill_in :description, with: ""
     fill_in :age, with: ""
-    fill_in :sex, with: ""
+    select 'F', :from => :sex
 
     click_button "Create Pet"
-    expect(page).to have_content("Age can't be blank and Sex can't be blank")
+    expect(page).to have_content("Age can't be blank and Description can't be blank")
     expect(current_path).to eq("/shelters/#{@shelter.id}/pets/new")
   end
 end
