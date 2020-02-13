8966cf4236f75805dceb33dddfbc5438ea3eb739
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-02-12 18:53:09 -0700

    Change the create method to add the pets through the application and not the opposite
    Co-Authored-By: agallant121 <52808022+agallant121@users.noreply.github.com>

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -7,27 +7,48 @@ class ApplicationsController < ApplicationController
     @application = Application.find(params[:id])
   end
 
-  def create
-    if !params[:pet_ids].nil? 
-      params[:pet_ids].each do |pet_id|
-        pet = Pet.find(pet_id)
-        if pet.applications.create(favorites_params).valid?
+  # def create
+  #   if !params[:pet_ids].nil? 
+  #     params[:pet_ids].each do |pet_id|
+  #       pet = Pet.find(pet_id)
+  #       if pet.applications.create(favorites_params).valid?
+  #         flash[:notice] = 'Your Application has been submitted successfully!'
+  #         favorites.update_favorites(params[:pet_ids])
+  #       else
+  #         flash[:notice] = 'You must complete the form in order to submit the application'
+  #         redirect_to "/applications/new"
+  #         return
+  #       end
+  #     end
+  #     redirect_to "/favorites"
+  #   else
+  #     flash[:notice] = 'You must complete the form in order to submit the application'
+  #     redirect_to "/applications/new"
+  #   end
+  # end
+
+    def create
+      if !params[:pet_ids].nil? 
+        application = Application.create(favorites_params)
+        if (application.valid?)
           flash[:notice] = 'Your Application has been submitted successfully!'
           favorites.update_favorites(params[:pet_ids])
+          params[:pet_ids].each do |pet_id|
+            pet = Pet.find(pet_id)
+            application.pets << pet 
+          end
+          redirect_to "/favorites" 
         else
           flash[:notice] = 'You must complete the form in order to submit the application'
           redirect_to "/applications/new"
           return
         end
+      else
+        flash[:notice] = 'You must complete the form in order to submit the application'
+        redirect_to "/applications/new"
       end
-      redirect_to "/favorites"
-    else
-      flash[:notice] = 'You must complete the form in order to submit the application'
-      redirect_to "/applications/new"
     end
-  end
-
-
+    
   private 
 
   def favorites_params 
