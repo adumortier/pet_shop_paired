78ba37efdf61e684fbf3a00255a90548b5615be3
Alex Gallant <agallant1201@gmail.com>
2020-02-12 18:33:53 -0700

    Add expectations for application show page when it has multiple pets on application
    Co-Authored-By: adumortier <adumortier@users.noreply.github.com>

--- spec/features/applications/application_approval_spec.rb
+++ spec/features/applications/application_approval_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'When I visit an application show page', type: :feature do
-  
+
   before :each do
     Shelter.destroy_all
     Pet.destroy_all
 
     @shelter_1 = Shelter.create!(name: "Abby's Shelter", address: "123 Maine Street", city: "Denver", state: "CO", zip: "80210")
-    
+
     @pet_1 = @shelter_1.pets.create!(
         image: 'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12234558/Chinook-On-White-03.jpg',
         name: "Penny",
@@ -24,24 +24,27 @@ RSpec.describe 'When I visit an application show page', type: :feature do
         description: "Good dog"
         )
 
-    @owner_info1 = {  :name => 'Alex', 
-                    :address => '123 Maine Ave', 
-                    :city => 'Paris', 
-                    :state => 'CO', 
-                    :zip => '80210', 
-                    :phone_number => '2077020720', 
+    @owner_info1 = {  :name => 'Alex',
+                    :address => '123 Maine Ave',
+                    :city => 'Paris',
+                    :state => 'CO',
+                    :zip => '80210',
+                    :phone_number => '2077020720',
                     :description => 'I will be a good pet owner, trust me.'}
-    pets = [@pet_1, @pet_2]                    
+    pets = [@pet_1, @pet_2]
     @application1  = Application.create(@owner_info1)
     @application1.pets << pets[0]
     @application1.pets << pets[1]
   end
 
   it "The application can be approved from the show page" do
-    
+
     visit "/applications/#{@application1.id}"
-    
-    within("span#pet_#{@pet_1.id}") do 
+
+    expect(page).to have_content(@pet_1.name)
+    expect(page).to have_content(@pet_2.name)
+
+    within("span#pet_#{@pet_1.id}") do
       click_link "Approve Application"
     end
 
@@ -50,8 +53,8 @@ RSpec.describe 'When I visit an application show page', type: :feature do
     expect(page).to have_content("On hold for #{@owner_info1[:name]}")
 
     visit "/applications/#{@application1.id}"
-    
-    within("span#pet_#{@pet_2.id}") do 
+
+    within("span#pet_#{@pet_2.id}") do
       click_link "Approve Application"
     end
 
