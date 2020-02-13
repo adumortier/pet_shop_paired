b12a44480bd2b30d0e1e58efe6879c822e849073
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-02-12 18:54:33 -0700

    Add the adopter field to the conditional to update the pet(s)
    Co-Authored-By: agallant121 <52808022+agallant121@users.noreply.github.com>

--- app/controllers/application_pets_controller.rb
+++ app/controllers/application_pets_controller.rb
@@ -8,10 +8,11 @@ class ApplicationPetsController < ApplicationController
   def update
     pet = Pet.find(params[:pet_id])
     if pet.adopted? == "Adoptable"
-      pet.update(adopted?: "Pending")
+      pet.update(adopted?: "Pending", adopter: params[:app_id].to_i)
+      flash[:approved] = 'Application Approved.'
       redirect_to "/pets/#{pet.id}"
     else
-      pet.update(adopted?: "Adoptable")
+      pet.update(adopted?: "Adoptable", adopter: nil)
       redirect_to "/applications/#{params[:app_id]}" 
     end  
   end
@@ -20,9 +21,26 @@ class ApplicationPetsController < ApplicationController
     pet_ids = params[:pet_ids]
     pet_ids.each do |id|
       pet = Pet.find(id)
-      pet.update(adopted?: "Pending")
+      if pet.adopted? == "Adoptable"
+        pet.update(adopted?: "Pending", adopter: params[:app_id].to_i)
+        flash[:approved] = "#{pet.name}'s application was approved."
+      else
+        pet.update(adopted?: "Adoptable", adopter: nil)
+      end  
     end
     redirect_to "/pets"
   end
 
 end
+
+
+
+#     pet_ids = params[:pet_ids]
+#     pet_ids.each do |id|
+#       pet = Pet.find(id)
+#       pet.update(adopted?: "Pending")
+#     end
+#     redirect_to "/pets"
+#   end
+
+# end
